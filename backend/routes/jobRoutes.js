dea4a7c48aa4e08ea8892a70c0373df568cfb965
mayank-vashishtt <145871109+mayank-vashishtt@users.noreply.github.com>
2024-08-27 10:06:08 +0530

    Update jobRoutes.js

--- backend/routes/jobRoutes.js
+++ backend/routes/jobRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const jobController = require('../controllers/JobController');
 
 router.post('/create', jobController.createJob);
-router.get('/', jobController.getAllJobs);
+router.get('/getAllJobs', jobController.getAllJobs);
 router.get('/:id', jobController.getJobById);
 router.put('/update/:id', jobController.updateJob);
 router.delete('/:id', jobController.deleteJob);
